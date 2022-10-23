10557a471e1bb8c5f1e716dcd6e89182824a05e2
Eligio Tapiador Jr <etapiador@gmail.com>
2022-10-23 10:02:20 +0800

    setup admin dashboard layout

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index']);
+
+});
