c4a4a95fd1a903d58185b24ac924005d69572d6b
Eligio Tapiador Jr <etapiador@gmail.com>
2022-10-23 11:10:28 +0800

    added Admin Middleware

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,8 +27,14 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected function authenticated(){
+        if(Auth::user()->role_as == '1'){
+            return redirect('admin/dashboard')->with('message','Welcome to Dashboard');
+        }else{
+            return redirect('/home')->with('status','Logged In Successfully');
+        }
+    }
     /**
      * Create a new controller instance.
      *
